ce76daf6eb6bfa5faaa3fd8ea5b2536ac1384a79
fernando <simon@example.com>
2025-11-22 16:33:41 -0700

    inicio de compute

--- RMI/src/Compute/Compute.java
+++ RMI/src/Compute/Compute.java
@@ -1,4 +1,8 @@
+package compute;
 
-public class Compute {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
+public interface Compute extends Remote {
+    <T> T executeTask(Task<T> t) throws RemoteException;
 }
