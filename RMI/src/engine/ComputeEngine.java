b3e88e6a8ca320206e2e252ded8ded74c90756b7
fernando <simon@example.com>
2025-11-22 17:34:00 -0700

    Engine

--- RMI/src/engine/ComputeEngine.java
+++ RMI/src/engine/ComputeEngine.java
@@ -1,3 +1,38 @@
-public class computeEngine {
+package engine;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import compute.Compute;
+import compute.Task;
+
+public class ComputeEngine implements Compute {
+
+    public ComputeEngine() {
+        super();
+    }
+
+    public <T> T executeTask(Task<T> t) {
+        return t.execute();
+    }
+
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(1099);
+            System.out.println("Registro RMI iniciado en puerto 1099");
+
+            Compute engine = new ComputeEngine();
+
+            Compute stub = (Compute)
+                    UnicastRemoteObject.exportObject(engine, 0);
+
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind("Compute", stub);
+
+            System.out.println("ComputeEngine listo");
+        } catch (Exception e) {
+            System.err.println("Error en ComputeEngine:");
+            e.printStackTrace();
+        }
+    }
 }
